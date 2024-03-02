630d444f2b6e4b7ee19f7986c8da22a1fdb2ed78
aparatmmm <130408698+aparatmmm@users.noreply.github.com>
2024-03-02 13:20:55 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Hello Vue!</h1>
+  <h2>Typy proste - ref</h2>
+  <p>Kurs: {{ courseName }}, uczestnicy: {{ courseParticipants }}</p>
+  <h2>Obiekty - reactive</h2>
+  <p>Kurs: {{  course.name }}, uczestnicy: {{ course.participants }}</p>
+</template>
+
+<script setup>
+  import { ref, reactive } from 'vue'
+  const courseName = ref('Bogate aplikacje internetowe')
+  const courseParticipants = ref(20)
+  const course = reactive({
+       name: 'Programowanie kreatywne',
+       participants: 10
+  })
+  courseParticipants.value++
+  course.participants++
+</script> vue
